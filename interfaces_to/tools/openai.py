fa1ecd5395c24c052ad21c8b72cfbc751a26e81b
Blair Hudson <blairhudson@me.com>
2024-08-01 00:32:07 +1000

    create_embedding

--- interfaces_to/tools/openai.py
+++ interfaces_to/tools/openai.py
@@ -48,11 +48,44 @@ class OpenAITool(FunctionSet):
             )
             response = completion.choices[0].message.content
 
-            print(f"Created completion using model {model} with prompt {prompt} and max tokens {max_tokens}. Response: {response}")
             return f"Created completion using model {model} with prompt {prompt} and max tokens {max_tokens}. Response: {response}"
+    
+    class CreateEmbedding(JSONSerializableFunction):
+
+        def __init__(self, tool):
+            super().__init__(tool)
+            self['type'] = "function"
+            self['function'] = {
+                "name": "create_embedding",
+                "description": "Create an embedding using the OpenAI API. Embeddings are vectors that represent the meaning of text.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "input": {
+                            "type": "string",
+                            "description": "The text to embed",
+                        },
+                        "model": {
+                            "type": "string",
+                            "description": "The model to use for completion. e.g. text-embedding-3-large or text-embedding-3-small",
+                        },
+                    },
+                    "required": ["input", "model"],
+                },
+            }
+
+        def create_embedding(self, input, model="text-embedding-3-large"):
+            client = OpenAI(api_key=self.tool.token)
+            embedding = client.embeddings.create(
+                model=model,
+                input=input
+            )
+            response = embedding.data[0].embedding
+
+            return f"Created embedding using model {model} with input {input}. Response: {response}"
 
     
-    def __init__(self, token=None, functions=['create_chat_completion']):
+    def __init__(self, token=None, functions=None):
         self.token = token
 
         # try and load the token from the environment
@@ -65,7 +98,11 @@ class OpenAITool(FunctionSet):
         # create a manual mapping of function names to classes
         self.functions_map = {
             'create_chat_completion': self.CreateChatCompletion,
+            'create_embedding': self.CreateEmbedding,
         }
+
+        if functions is None:
+            functions = self.functions_map.keys()
 
         # instantiate each class and add it to the class instance for the functions in the constructor
         self.functions = [self.functions_map[function](self) for function in functions]
