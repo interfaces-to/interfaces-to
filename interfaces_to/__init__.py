19caf7da89530ccf180356e1467571d32e29dd14
Blair Hudson <blairhudson@me.com>
2024-08-02 22:57:26 +1000

    add: lazy imports to avoid hard dependencies for tools

--- interfaces_to/__init__.py
+++ interfaces_to/__init__.py
@@ -1,7 +1,20 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from .tools.slack import Slack
-from .tools.openai import OpenAITool as OpenAI
-from .tools.self import Self
-from .utils import run, running, tools
+import sys
+from .utils import run, running, tools, LazyImport
+
+# all tools are imported lazily to avoid hard package dependencies
+tool_classes = [
+    ('Slack', '.tools.slack', 'Slack'),
+    ('OpenAI', '.tools.openai', 'OpenAITool'),
+    ('Notion', '.tools.notion', 'Notion'),
+    ('Self', '.tools.self', 'Self'),
+]
+
+# create a module for each tool
+for name, location, class_name in tool_classes:
+    sys.modules[__name__ + '.' + name] = LazyImport(location, class_name)
+
+# only export what is needed
+__all__ = [class_name for _, _, class_name in tool_classes] + ['tools', 'run', 'running']
