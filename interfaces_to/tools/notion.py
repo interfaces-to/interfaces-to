bec23cfd0843895de5694d1b3157680b9e928d62
Blair Hudson <blairhudson@users.noreply.github.com>
2024-10-30 23:14:05 +1100

    Use the more recent tools approach for the older tools
    Update `Notion` and `OpenAI` tools to use `@callable_function` and `@tool_auth` decorators.
    
    * **Notion Tool:**
      - Add `@tool_auth` decorator with `token_env_name='NOTION_TOKEN'` to the `Notion` class.
      - Replace manual mapping of function names to classes with `@callable_function` decorator for each function.
      - Update `__init__` method to remove manual mapping and use `FunctionSet` base class.
    
    * **OpenAI Tool:**
      - Add `@tool_auth` decorator with `token_env_name='OPENAI_API_KEY'` to the `OpenAI` class.
      - Replace manual mapping of function names to classes with `@callable_function` decorator for each function.
      - Update `__init__` method to remove manual mapping and use `FunctionSet` base class.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/interfaces-to/interfaces-to?shareId=XXXX-XXXX-XXXX-XXXX).

--- interfaces_to/tools/notion.py
+++ interfaces_to/tools/notion.py
@@ -1,216 +1,71 @@
-from ..bases import FunctionSet, JSONSerializableFunction
-import os
+from ..bases import FunctionSet
+from ..utils import callable_function, tool_auth
 from notion_client import Client, APIErrorCode, APIResponseError
 
-
+@tool_auth(token_env_name='NOTION_TOKEN')
 class Notion(FunctionSet):
 
-    class List(JSONSerializableFunction):
-        def __init__(self, tool):
-            super().__init__(tool)
-            self['type'] = "function"
-            self['function'] = {
-                "name": "list_notion",
-                "description": "List all pages and databases in Notion and optionally filter by title",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "title": {
-                            "type": "string",
-                            "description": "A string to filter the results by title",
-                        },
-                    },
-                    "required": [],
-                },
-            }
+    @callable_function
+    def list_notion(self, title=""):
+        """
+        List all pages and databases in Notion and optionally filter by title
+        
+        :param title: A string to filter the results by title
+        """
+        notion = Client(auth=self.token)
 
-        def list_notion(self, title=""):
+        try:
+            response = notion.search(query=title)
 
-            notion = Client(auth=os.environ["NOTION_TOKEN"])
+            return f"Listing everything in Notion{(', filtered by: ' + title) if title != '' else ''}. Response: {response}"
+        except APIResponseError as e:
+            return f"Error: {e}"
 
-            try:
-                response = notion.search(query=title)
+    @callable_function
+    def query_notion_database(self, database_id: str):
+        """
+        Query a database in Notion
+        
+        :param database_id: The ID of the database to query. To get a database ID, use the list_notion function.
+        """
+        notion = Client(auth=self.token)
+        try:
+            response = notion.databases.query(database_id=database_id)
 
-                return f"Listing everything in Notion{(', filtered by: ' + title) if title != '' else ''}. Response: {response}"
-            except APIResponseError as e:
-                return f"Error: {e}"
+            return f"Querying database {database_id} in Notion. Response: {response}"
+        except APIResponseError as e:
+            return f"Error: {e}"
 
-    class DatabaseQuery(JSONSerializableFunction):
-        def __init__(self, tool):
-            super().__init__(tool)
-            self['type'] = "function"
-            self['function'] = {
-                "name": "query_notion_database",
-                "description": "Query a database in Notion",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "database_id": {
-                            "type": "string",
-                            "description": "The ID of the database to query. To get a database ID, use the list_notion function.",
-                        }
-                    },
-                    "required": ["database_id"],
-                },
-            }
+    @callable_function
+    def create_notion_page(self, parent_id: dict, children: dict = {}, properties: dict = {}):
+        """
+        Create a page in Notion
+        
+        :param parent_id: The ID of the parent page to create the new page under.
+        :param children: The array of block objects of the new page
+        :param properties: The properties of the new page
+        """
+        notion = Client(auth=self.token)
 
-        def query_notion_database(self, database_id):
-            notion = Client(auth=os.environ["NOTION_TOKEN"])
-            try:
+        try:
+            created_page = notion.pages.create(
+                parent=parent_id, children=children, properties=properties)
 
-                response = notion.databases.query(database_id=database_id)
+            return f"Creating page in Notion under {parent_id}. Response: {created_page}"
+        except APIResponseError as e:
+            return f"Error: {e}"
 
-                return f"Querying database {database_id} in Notion. Response: {response}"
+    @callable_function
+    def read_notion_page(self, page_id: str):
+        """
+        Read the contents of a page in Notion
+        
+        :param page_id: The ID of the page to read.
+        """
+        notion = Client(auth=self.token)
+        try:
+            response = notion.pages.retrieve(page_id=page_id)
 
-            except APIResponseError as e:
-                return f"Error: {e}"
-
-    class CreatePage(JSONSerializableFunction):
-        def __init__(self, tool):
-            super().__init__(tool)
-            self['type'] = "function"
-            self['function'] = {
-                "name": "create_notion_page",
-                "description": "Create a page in Notion",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "parent_id": {
-                            "type": "object",
-                            "description": "The ID of the parent page to create the new page under.",
-                            "properties": {
-                                "database_id": {
-                                    "type": "string",
-                                    "description": "The ID of the database to create the page under",
-                                },
-                            }
-
-                        },
-                        "children": {
-                            "type": "array",
-                            "description": "The array of block objects of the new page",
-                            "items": {
-                                "type": "object",
-                                "properties": {
-                                    "object": {
-                                        "type": "string",
-                                        "description": "The type of block object. e.g. \"block\" or \"page\"",
-                                    },
-                                    "type": {
-                                        "type": "string",
-                                        "description": "The type of block. e.g. \"heading_1\" or \"paragraph\"",
-                                    },
-                                    "paragraph": {
-                                        "type": "object",
-                                        "description": "The paragraph content of the block",
-                                        "properties": {
-                                            "rich_text": {
-                                                "type": "array",
-                                                "description": "The rich text content of the paragraph",
-                                                "items": {
-                                                    "type": "object",
-                                                    "properties": {
-                                                        "type": {
-                                                            "type": "string",
-                                                            "description": "The type of content. e.g. \"text\" or \"mention\"",
-                                                        },
-                                                        "text": {
-                                                            "type": "object",
-                                                            "description": "The text content",
-                                                            "properties": {
-                                                                "content": {
-                                                                    "type": "string",
-                                                                    "description": "The actual text content",
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        },
-                        "properties": {
-                            "type": "object",
-                            "description": "The properties of the new page",
-
-                            "properties": {
-                                "title": {
-                                    "type": "object",
-                                    "description": "The title of the new page",
-                                },
-                            },
-
-                        },
-                    },
-                    "required": ["parent_id", "children", "properties"],
-                },
-            }
-
-        def create_notion_page(self, parent_id, children={}, properties={}):
-
-            notion = Client(auth=os.environ["NOTION_TOKEN"])
-
-            try:
-                created_page = notion.pages.create(
-                    parent=parent_id, children=children, properties=properties)
-
-                return f"Creating page in Notion under {parent_id}. Response: {created_page}"
-            except APIResponseError as e:
-                return f"Error: {e}"
-    
-    class ReadPage(JSONSerializableFunction):
-        def __init__(self, tool):
-            super().__init__(tool)
-            self['type'] = "function"
-            self['function'] = {
-                "name": "read_notion_page",
-                "description": "Read the contents of a page in Notion",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "page_id": {
-                            "type": "string",
-                            "description": "The ID of the page to read.",
-                        }
-                    },
-                    "required": ["page_id"],
-                },
-            }
-
-        def read_notion_page(self, page_id):
-            notion = Client(auth=os.environ["NOTION_TOKEN"])
-            try:
-                response = notion.pages.retrieve(page_id=page_id)
-
-                return f"Reading page {page_id} in Notion. Response: {response}"
-            except APIResponseError as e:
-                return f"Error: {e}"
-
-    def __init__(self, token=None, functions=None):
-        self.token = token
-
-        # try and load token from os.environ["NOTION_TOKEN"]
-        if token is None:
-            try:
-                self.token = os.environ["NOTION_TOKEN"]
-            except KeyError:
-                raise ValueError(
-                    "No token provided and NOTION_TOKEN not found in environment variables")
-
-        # create a manual mapping of function names to classes
-        self.functions_map = {
-            'search_notion': self.List,
-            'query_notion_database': self.DatabaseQuery,
-            'create_notion_page': self.CreatePage,
-            'read_notion_page': self.ReadPage,
-        }
-
-        if functions is None:
-            functions = self.functions_map.keys()
-
-        # instantiate each class and add it to the class instance for the functions in the constructor
-        self.functions = [self.functions_map[function]
-                          (self) for function in functions]
+            return f"Reading page {page_id} in Notion. Response: {response}"
+        except APIResponseError as e:
+            return f"Error: {e}"
--- interfaces_to/tools/openai.py
+++ interfaces_to/tools/openai.py
@@ -1,108 +1,46 @@
-from ..bases import FunctionSet, JSONSerializableFunction
+from ..bases import FunctionSet
+from ..utils import callable_function, tool_auth
 from openai import OpenAI as OpenAISDK
-import os
 
+@tool_auth(token_env_name='OPENAI_API_KEY')
 class OpenAI(FunctionSet):
 
-    class CreateChatCompletion(JSONSerializableFunction):
+    @callable_function
+    def create_chat_completion(self, prompt: str, model: str = "gpt-4o", system_prompt: str = "You are a helpful assistant.", max_tokens: int = None):
+        """
+        Create a completion using the OpenAI API
+        
+        :param prompt: The prompt to use for completion
+        :param model: The model to use for completion. e.g. gpt-4o
+        :param system_prompt: The optional system prompt to use to guide the model. e.g. You are a helpful assistant.
+        :param max_tokens: The maximum number of tokens to generate
+        """
+        client = OpenAISDK(api_key=self.token)
+        completion = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=max_tokens
+        )
+        response = completion.choices[0].message.content
 
-        def __init__(self, tool):
-            super().__init__(tool)
-            self['type'] = "function"
-            self['function'] = {
-            "name": "create_chat_completion",
-            "description": "Create a completion using the OpenAI API",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "prompt": {
-                        "type": "string",
-                        "description": "The prompt to use for completion",
-                    },
-                    "model": {
-                        "type": "string",
-                        "description": "The model to use for completion. e.g. gpt-4o",
-                    },
-                    "system_prompt": {
-                        "type": "string",
-                        "description": "The optional system prompt to use to guide the model. e.g. You are a helpful assistant.",
-                    },
-                    "max_tokens": {
-                        "type": "integer",
-                        "description": "The maximum number of tokens to generate",
-                    },
-                },
-                "required": ["prompt"],
-            },
-        }
-            
-        def create_chat_completion(self, prompt, model="gpt-4o", system_prompt="You are a helpful assistant.", max_tokens=None):
-            client = OpenAISDK(api_key=self.tool.token)
-            completion = client.chat.completions.create(
-                model=model,
-                messages=[
-                    {"role": "system", "content": system_prompt},
-                    {"role": "user", "content": prompt},
-                ],
-                max_tokens=max_tokens
-            )
-            response = completion.choices[0].message.content
-
-            return f"Created completion using model {model} with prompt {prompt} and max tokens {max_tokens}. Response: {response}"
+        return f"Created completion using model {model} with prompt {prompt} and max tokens {max_tokens}. Response: {response}"
     
-    class CreateEmbedding(JSONSerializableFunction):
+    @callable_function
+    def create_embedding(self, input: str, model: str = "text-embedding-3-large"):
+        """
+        Create an embedding using the OpenAI API. Embeddings are vectors that represent the meaning of text.
+        
+        :param input: The text to embed
+        :param model: The model to use for completion. e.g. text-embedding-3-large or text-embedding-3-small
+        """
+        client = OpenAISDK(api_key=self.token)
+        embedding = client.embeddings.create(
+            model=model,
+            input=input
+        )
+        response = embedding.data[0].embedding
 
-        def __init__(self, tool):
-            super().__init__(tool)
-            self['type'] = "function"
-            self['function'] = {
-                "name": "create_embedding",
-                "description": "Create an embedding using the OpenAI API. Embeddings are vectors that represent the meaning of text.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "input": {
-                            "type": "string",
-                            "description": "The text to embed",
-                        },
-                        "model": {
-                            "type": "string",
-                            "description": "The model to use for completion. e.g. text-embedding-3-large or text-embedding-3-small",
-                        },
-                    },
-                    "required": ["input", "model"],
-                },
-            }
-
-        def create_embedding(self, input, model="text-embedding-3-large"):
-            client = OpenAISDK(api_key=self.tool.token)
-            embedding = client.embeddings.create(
-                model=model,
-                input=input
-            )
-            response = embedding.data[0].embedding
-
-            return f"Created embedding using model {model} with input {input}. Response: {response}"
-
-    
-    def __init__(self, token=None, functions=None):
-        self.token = token
-
-        # try and load the token from the environment
-        if token is None:
-            try:
-                self.token = os.environ['OPENAI_API_KEY']
-            except KeyError:
-                raise ValueError("No token provided and OPENAI_API_KEY not found in environment")
-
-        # create a manual mapping of function names to classes
-        self.functions_map = {
-            'create_chat_completion': self.CreateChatCompletion,
-            'create_embedding': self.CreateEmbedding,
-        }
-
-        if functions is None:
-            functions = self.functions_map.keys()
-
-        # instantiate each class and add it to the class instance for the functions in the constructor
-        self.functions = [self.functions_map[function](self) for function in functions]
+        return f"Created embedding using model {model} with input {input}. Response: {response}"
